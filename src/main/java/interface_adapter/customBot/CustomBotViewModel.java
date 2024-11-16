be9636f05290c4503262dfde43345f99e3a74d93
ThomasSwanick <thomas.swanick@mail.utoronto.ca>
2024-11-16 10:28:09 -0500

    adding set state in view model

--- src/main/java/interface_adapter/customBot/CustomBotViewModel.java
+++ src/main/java/interface_adapter/customBot/CustomBotViewModel.java
@@ -2,8 +2,12 @@ package interface_adapter.customBot;
 
 import interface_adapter.ViewModel;
 
+/**
+ * The View Model for the Custom Bot View.
+ */
 public class CustomBotViewModel extends ViewModel<CustomBotState> {
     public CustomBotViewModel() {
         super("customBot");
+        setState(new CustomBotState());
     }
 }
