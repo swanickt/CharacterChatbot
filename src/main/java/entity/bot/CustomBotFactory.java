2c3921443eded1d3475608185b4e8a2994f7dc6a
NKwyk <wyk5204@163.com>
2024-11-24 15:24:22 -0500

    for a small change in override stuff

--- src/main/java/entity/bot/CustomBotFactory.java
+++ src/main/java/entity/bot/CustomBotFactory.java
@@ -5,13 +5,14 @@ package entity.bot;
  */
 public class CustomBotFactory implements BotFactory {
 
-    public CustomBot create(String name, String occupation) {
-        return new CustomBot(name, occupation);
-    }
-
     // Just need the below method so it can implement botfactory.
+    @Override
     public CustomBot create() {
         return new CustomBot("AI", "helpful assistant");
+    }
+
+    public CustomBot create(String name, String occupation) {
+        return new CustomBot(name, occupation);
     }
 
 }
